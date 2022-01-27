0c416b8864f79f5a89efba4148d700232e023631
iamgaby <gaby.es.iaa@gmail.com>
2022-01-27 13:53:02 -0500

    Update index.js to call async functions from DataBoard class file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import './scss/style.scss';
+import DataBoard from './dataBoard.js';
 
+const data = new DataBoard();
 const refreshBtn = document.querySelector('#btn__refresh');
 const inputForm = document.querySelector('#form__input');
 const inputUser = document.querySelector('#inp__user');
 const inputScore = document.querySelector('#inp__score');
 const scoreList = document.querySelector('#score__list');
 const formMessage = document.querySelector('#form__message');
-const basicURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
-let idGame = '';
 
-const displayError = (err) => {
-  formMessage.textContent = err;
-  formMessage.classList.add('error-message', 'visible');
+const displayMessage = (err) => {
+  if (err) {
+    formMessage.textContent = err;
+    formMessage.classList.add('error-message');
+  }
+  formMessage.classList.add('visible');
   setTimeout(() => {
     formMessage.classList.remove('visible');
   }, 5000);
@@ -25,92 +28,54 @@ const validateForm = () => {
     formMessage.classList.add('error-message');
     return false;
   }
-  if (inputScore.value === '' || +inputScore.value <= 0) {
+  if (inputScore.value === '' || inputScore.value <= 0) {
     inputScore.focus();
-    formMessage.textContent = 'Score field is required and must be a postitive number.';
+    formMessage.textContent = 'Score field must be a positive number.';
     formMessage.classList.add('error-message');
     return false;
   }
   return true;
 };
 
-const buildScoreList = (arr) => {
-  scoreList.innerHTML = '';
-  arr.forEach((el) => {
-    const listItem = document.createElement('li');
-    listItem.classList.add('score__list-item');
-    listItem.innerHTML = `<p class="score__item-name">${el.user}</p>
+const getScoresList = () => {
+  data.getScores().then((elements) => {
+    scoreList.innerHTML = '';
+    elements.forEach((el) => {
+      const listItem = document.createElement('li');
+      listItem.classList.add('score__list-item');
+      listItem.innerHTML = `<p class="score__item-name">${el.user}</p>
           <span class="score__item-score">${el.score}</span>`;
-    scoreList.appendChild(listItem);
+      scoreList.appendChild(listItem);
+    });
   });
 };
 
-const getScores = async () => {
-  let data = [];
-  try {
-    const response = await fetch(`${basicURL}games/${idGame}/scores/`);
-    const scores = await response.json();
-    data = scores.result;
-    data.sort((a, b) => b.score - a.score);
-
-    if (data.length === 0) {
-      data = [{ user: `Game with ID: ${idGame} is empty. </br> Add a score`, score: '' }];
-    }
-    buildScoreList(data);
-  } catch {
-    data = [{ user: 'Data is not available for now. Try again later.', score: '' }];
-    buildScoreList(data);
-  }
+const addNewScore = () => {
+  data.addScore(inputUser.value, inputScore.value).then(() => {
+    if (data.message !== 'Successful') displayMessage(data.message);
+    else getScoresList();
+  });
 };
 
-const addScore = async (user1, score1) => {
-  try {
-    const response = await fetch(`${basicURL}games/${idGame}/scores/`, {
-      method: 'POST',
-      body: JSON.stringify({ user: user1, score: +score1 }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
-    await response.json();
-    getScores();
-  } catch (err) {
-    displayError('The score could not be saved. Try it again later.');
-  }
-};
-
-const addGame = async () => {
-  try {
-    const response = await fetch(`${basicURL}games/`, {
-      method: 'POST',
-      body: JSON.stringify({ name: 'My cool new game' }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
-    const data = await response.json();
-    idGame = data.result.slice(14, 34);
-    getScores();
-  } catch (err) {
-    displayError('The game could not be created. Try it again later.');
-  }
+const addNewGame = () => {
+  data.addGame().then(() => {
+    if (data.message !== 'Successful') displayMessage(data.message);
+    else getScoresList();
+  });
 };
 
 // events listener
-refreshBtn.addEventListener('click', getScores);
+refreshBtn.addEventListener('click', getScoresList);
 
 inputForm.addEventListener('submit', (e) => {
   e.preventDefault();
   if (validateForm()) {
-    addScore(inputUser.value, inputScore.value);
+    addNewScore();
     inputUser.value = '';
     inputScore.value = '';
     formMessage.textContent = 'Score added successfully';
   }
-  formMessage.classList.add('visible');
-  setTimeout(() => {
-    formMessage.classList.remove('visible');
-  }, 5000);
+  displayMessage();
 });
 
-window.addEventListener('load', addGame);
+window.addEventListener('load', addNewGame);
